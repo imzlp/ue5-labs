f805beefc134eac1a6f85d8d7a2815cbb620d13b
hxhb <hxhb@live.com>
2021-02-02 12:14:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,20 @@ new Docute({
       title: 'Home',
       link: '/'
     }
+  ],
+  sidebar: [
+    {
+      title: 'Feeds',
+      links: [
+        {
+          title: 'Business',
+          link: '/Business'
+        },
+        {
+          title: 'Technology',
+          link: '/Technology'
+        }
+      ]
+    }
   ]
 })
